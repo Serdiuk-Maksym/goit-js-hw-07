37f995a07cf8dd63be61fc56a8d64a090441e8f3
Serdiuk-Maksym <serdyukmaksimaleksandrovich@gmail.com>
2023-05-19 04:24:02 +0300

    second fix

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+document.addEventListener('DOMContentLoaded', () => {
+  const lightbox = new SimpleLightbox('.gallery');
+
+  const galleryContainer = document.querySelector('.gallery');
+
+  galleryItems.forEach((item) => {
+    const galleryLink = document.createElement('a');
+    galleryLink.href = item.original;
+    galleryLink.classList.add('gallery__item');
+
+    const galleryImage = document.createElement('img');
+    galleryImage.src = item.preview;
+    galleryImage.alt = item.description;
+    galleryImage.classList.add('gallery__image');
+
+    galleryLink.appendChild(galleryImage);
+    galleryContainer.appendChild(galleryLink);
+  });
+
+  const lightboxImages = galleryContainer.querySelectorAll('.gallery__image');
+
+  lightboxImages.forEach((image) => {
+    image.addEventListener('click', () => {
+      setTimeout(() => {
+        const caption = document.createElement('div');
+        caption.classList.add('caption');
+        caption.textContent = image.alt;
+        image.parentNode.appendChild(caption);
+      }, 250);
+    });
+  });
+});
